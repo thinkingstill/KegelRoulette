dbf56ac2a6275fe8f2c95e980191a82e8c13b2fa
wlh <nb@wlh.com>
2025-11-08 21:57:32 +0800

    bugfix:gemini2.5 pro再来修复一次

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,15 +1,14 @@
 // app/global-error.tsx
 "use client";
-export const runtime = "edge";
 
 import ClientError from "./client-error";
 
-export default function GlobalError(props: any) {
-  return (
-    <html>
-      <body>
-        <ClientError {...props} />
-      </body>
-    </html>
-  );
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return <ClientError error={error} reset={reset} />;
 }
